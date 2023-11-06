64b619738e275584eb104bc40f9b69fdbff09a9c
fatmaamr22 <83840226+fatmaamr22@users.noreply.github.com>
2023-11-06 16:09:51 +0200

    Add files via upload

---
+++ HTTP-Server-main/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HTTPServer
+{
+    class Logger
+    {
+        //static StreamWriter sr = new StreamWriter("log.txt");
+        public static void LogException(Exception ex)
+        {
+            // TODO: Create log file named log.txt to log exception details in it
+            //Datetime:
+            //message:
+            // for each exception write its details associated with datetime 
+            
+            var date = DateTime.Now.ToShortDateString();
+            using (var tw = new StreamWriter("log.txt", true))
+            {
+                tw.WriteLine("Datetime:" + date + ", message:" + ex.Message);
+            }
+        }
+    }
+}
